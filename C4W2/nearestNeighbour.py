14eb1a98992fe7399df1b2e9c1f60f114e9c56f4
howardx <howard@example.com>
2016-07-15 21:22:51 -0500

    scipy sparse matrix

--- C4W2/nearestNeighbour.py
+++ C4W2/nearestNeighbour.py
@@ -2,3 +2,45 @@ import sframe                            # see below for install instruction
 import matplotlib.pyplot as plt          # plotting
 import numpy as np                       # dense matrices
 from scipy.sparse import csr_matrix      # sparse matrices
+
+from sklearn.neighbors import NearestNeighbors
+
+import unpack_dict as ud
+
+wiki = sframe.SFrame('people_wiki.gl/')
+wiki = wiki.add_row_number()             # add row number, starting at 0
+
+#print wiki.column_names()
+
+def load_sparse_csr(filename):
+    loader = np.load(filename)
+    data = loader['data']
+    indices = loader['indices']
+    indptr = loader['indptr']
+    shape = loader['shape']
+    
+    return csr_matrix( (data, indices, indptr), shape)
+    
+word_count = load_sparse_csr('people_wiki_word_count.npz')
+map_index_to_word = sframe.SFrame('people_wiki_map_index_to_word.gl/')
+
+print map_index_to_word.column_names()
+print map_index_to_word.head()
+
+model = NearestNeighbors(metric='euclidean', algorithm='brute')
+model.fit(word_count)
+
+# find where Obaba's article is listed in Input data set
+#print wiki[wiki['name'] == 'Barack Obama']
+
+# 1st arg: word count vector, record 35817 is where Obama's article is
+distances, id = model.kneighbors(word_count[35817], n_neighbors=10) 
+
+knn = sframe.SFrame({
+                            'distance' : distances.flatten(),
+                            'id' : indices.flatten()
+                    })
+
+print wiki.join(knn, on = 'id').sort('distance')[['id', 'name', 'distance']]
+
+wiki['word_count'] = ud.unpack_dict(word_count, map_index_to_word)
---
+++ C4W2/unpack_dict.py
@@ -0,0 +1,72 @@
+import sframe
+from scipy.sparse import csr_matrix
+import numpy as np
+
+
+def load_sparse_csr(filename):
+    loader = np.load(filename)
+    data = loader['data']
+    indices = loader['indices']
+    indptr = loader['indptr']
+    shape = loader['shape']
+    
+    return csr_matrix( (data, indices, indptr), shape)
+    
+
+def unpack_dict(sparse_matrix, sframe_lookup):
+    table = list(sframe_lookup.sort('index')['category'])
+    data = sparse_matrix.data
+    indices = sparse_matrix.indices
+    indptr = sparse_matrix.indptr
+    
+    num_row = sparse_matrix.shape[0] # number of rows
+
+    return [
+            { k : v for k, v in zip(
+                [ table[word_id] for word_id in indices[ indptr[i] : indptr[i+1] ] ],
+                data[ indptr[i] : indptr[i+1] ].tolist()
+            )}
+            for i in xrange(num_row)
+           ]
+
+# Tests -
+def unitTest():
+    wordCount = load_sparse_csr('people_wiki_word_count.npz')
+    #map_index_to_word = sframe.SFrame('people_wiki_map_index_to_word.gl/')
+
+    # wiki['word_count'] = ud.unpack_dict(wordCount, map_index_to_word)
+    print type(wordCount.data)
+    print wordCount.data.shape # non-zero cell value in sparse matrix
+    
+    print type(wordCount.indices)
+    print wordCount.indices.shape # non-zero row index in sparse matrix
+    
+    print type(wordCount.indptr)
+    print wordCount.indptr.shape # non-zero column/cell index in sparse matrix
+    
+    i = 0
+    non_0_col_indices_given_row_0_1 = wordCount.indices[wordCount.indptr[i] : wordCount.indptr[i+1]]
+    
+    i = 0
+    non_0_col_vals_given_row_0_1 = wordCount.data[ wordCount.indptr[i] : wordCount.indptr[i+1]]
+    
+    print len(non_0_col_indices_given_row_0_1) == len(non_0_col_vals_given_row_0_1)
+    
+    i = 1
+    non_0_col_indices_given_row_1_3 = wordCount.indices[wordCount.indptr[i] : wordCount.indptr[i+2]]
+    i = 0
+    non_0_col_indices_given_row_0_3 = wordCount.indices[wordCount.indptr[i] : wordCount.indptr[i+3]]
+    
+    '''
+    in slice of non-zero rows (row vectors) 0 to 3 (end exclusive slice), 
+    the number of non-zero values/cells/columns equals to the sum of
+    
+    -number of nonZero values/cells/columns in slice of nonZero rows (row vectors) 0 to 1 (end exclusive slice)
+    AND
+    -number of nonZero values/cells/columns in slice of nonZero rows (row vectors) 1 to 3 (end exclusive slice)
+    '''
+    print len(non_0_col_indices_given_row_0_3) == ( len(non_0_col_indices_given_row_0_1) +
+                                                    len(non_0_col_indices_given_row_1_3) )
+
+
+unitTest()
